22fe78f25b0e3a437ef494f36a379bcc377e678f
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-02-22 02:18:49 +0300

    chore: Register WebhookModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WebhookModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
